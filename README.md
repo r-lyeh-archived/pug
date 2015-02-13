aa070b007c5a1e8e08aeb3459e42c9081beadcdf
r-lyeh <rlyeh.nospam@gmail.com>
2015-02-13 14:22:24 +0100

    rewrote pugify tool; removed spot lib dependency; updated other minor details;

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<img align="right" src="http://www.hdwallpaperstock.eu/wallpapers/funny_pug_dog-2880x1800.jpg" width="320" alt="@todo, disclaimer here: unknown author, pending license http://i.ytimg.com/vi/pzPxhaYQQK8/hqdefault.jpg" />
+<img align="right" src="https://raw.github.com/r-lyeh/depot/master/pug.png" width="320" alt="@todo, disclaimer here: unknown author, pending credits http://i.ytimg.com/vi/pzPxhaYQQK8/hqdefault.jpg" />
 ### PUG
 ####
 
@@ -20,11 +20,11 @@
 | Original image    | Lossy color | Lossless alpha | Pug image |
 | :-------------: |:-------------:| :-----:| :-----: |
 | ![image](https://raw.github.com/r-lyeh/pug/master/images/panda.png) | ![image](https://raw.github.com/r-lyeh/pug/master/images/panda.pug.color.jpg) | ![image](https://raw.github.com/r-lyeh/pug/master/images/panda.pug.alpha.png) | ![image](https://raw.github.com/r-lyeh/pug/master/images/panda.pug.rebuilt.png) |
-| PNG Image, 56 KiB | JPG layer, Q=75% | PNG layer, 8-bit | PUG Image, 13 KiB |
+| PNG Image, 56 KiB | JPG plane, Q=75% | PNG plane, 8-bit | PUG Image, 13 KiB |
 | ![image](https://raw.github.com/r-lyeh/pug/master/images/dices.png) | ![image](https://raw.github.com/r-lyeh/pug/master/images/dices.pug.color.jpg) | ![image](https://raw.github.com/r-lyeh/pug/master/images/dices.pug.alpha.png) | ![image](https://raw.github.com/r-lyeh/pug/master/images/dices.pug.rebuilt.png) |
-| PNG Image, 213 KiB | JPG layer, Q=75% | PNG layer, 8-bit | PUG Image, 73 KiB |
+| PNG Image, 213 KiB | JPG plane, Q=75% | PNG plane, 8-bit | PUG Image, 73 KiB |
 | ![image](https://raw.github.com/r-lyeh/pug/master/images/babytux.png) | ![image](https://raw.github.com/r-lyeh/pug/master/images/babytux.pug.color.jpg) | ![image](https://raw.github.com/r-lyeh/pug/master/images/babytux.pug.alpha.png) | ![image](https://raw.github.com/r-lyeh/pug/master/images/babytux.pug.rebuilt.png) |
-| PNG Image, 40 KiB | JPG layer, Q=75% | PNG layer, 8-bit | PUG Image, 20 KiB |
+| PNG Image, 40 KiB | JPG plane, Q=75% | PNG plane, 8-bit | PUG Image, 20 KiB |
 
 #### PUG file format specification
 - RGBA32 input image is split into RGB24.jpg and A8.png images, then glued together with a footer.
@@ -32,9 +32,9 @@
 ```c++
 RGBA32.pug = [RGB24.jpg] + [optional padding] + [A8.png] + [optional padding] + [footer]
 
-- [?? bytes]  lossy color chunk (JPG stream)
+- [?? bytes]  lossy color plane (JPG stream)
 - [?? bytes]  lossy color padding (optional)
-- [?? bytes]  lossless alpha chunk (PNG stream)
+- [?? bytes]  lossless alpha plane (PNG stream)
 - [?? bytes]  lossless alpha padding (optional)
 - [ 4 bytes]  footer chunk - sizeof( color + color_padding ) (little endian)
 - [ 4 bytes]  footer chunk - sizeof( alpha + alpha_padding ) (little endian)
@@ -60,10 +60,9 @@ rgba.at(w,h) = rgb.at(w,h) * pixel(255,255,255,0) + alpha.at(w,h) * pixel(0,0,0,
 
 #### Minimal provided tools
 - [pugify.exe](https://raw.github.com/r-lyeh/pug/master/tools/pugify.exe), which does PNG <=> PUG conversions ([source code](tools/pugify.cc))
-- [pugview.exe](https://raw.github.com/r-lyeh/pug/master/tools/pugview.exe), which previews image files (including pug files) ([source code](tools/pugview.cc))
 
 #### Notes
-- Pugify uses tiny image encoders that may lead to sub-optimal JPG/PNG file sizes. Therefore, Pugify may generate sub-optimal (but fully working) PUG files. If you plan to use Pugify in production scenarios consider integrating better image encoders before merging RGB24 and A8 files both together.
+- Pugify uses tiny image encoders that may lead to sub-optimal JPG/PNG file sizes. Therefore, Pugify may generate sub-optimal (but fully compliant) PUG files. If you plan to use Pugify in production scenarios consider integrating better image encoders before merging RGB24 and A8 planes both together.
 - [panda.png](images/panda.png) taken from http://tinypng.com website.
 - [dices.png](images/dices.png) taken from POV-Ray source code (CCASA30U license), [source](http://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png)
 - [babytux.png](images/babytux.png) by Fizyplankton (public domain), [source](http://www.minecraftwiki.net/images/8/85/Fizyplankton.png)
@@ -71,15 +70,9 @@ rgba.at(w,h) = rgb.at(w,h) * pixel(255,255,255,0) + alpha.at(w,h) * pixel(0,0,0,
 
 #### Appendix: on compiling provided tools
 ```
-git submodule init
-git submodule update
-
-cl pugview.cc -I deps deps\spot\spot*.c* /Ox /Oy /MT /DNDEBUG /link setargv.obj
 cl pugify.cc  -I deps deps\spot\spot*.c* /Ox /Oy /MT /DNDEBUG
 
-pugify.exe deps\spot\images\panda.png panda.pug 75
-pugview.exe panda.pug
-
-pugify.exe panda.pug panda.png
-pugview.exe panda.png
+pugify.exe panda.png panda.pug 75
+pugify.exe panda.pug panda.rebuilt.png
+start panda.rebuilt.png
 ```
--- tools/pugify.cc
+++ tools/pugify.cc
@@ -1,113 +1,211 @@
 // pugify: png<=>pug conversion. public domain.
 // - rlyeh.
 
+#include <stddef.h>
+#include <string.h>
 #include <cctype>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
-#include "spot/spot.hpp"
+#include "deps/lodepng.h"
+#include "deps/lodepng.cpp"
+#include "deps/jpge.h"
+#include "deps/jpge.cpp"
+#include "deps/stb_image.c"
 
-#define VERSION "1.0.1"
+#define VERSION "1.0.2"
 
-std::string readfile( const std::string &stream ) {
-    std::ifstream ifs( stream, std::ios::binary );
-    std::stringstream ss;
-    ss << ifs.rdbuf();
-    return ss.str();
-}
+struct image {
+    unsigned w, h;
+    std::vector<unsigned char> pixels;
+};
 
-std::string mkfooter( const std::string &color24, const std::string &alpha08 ) {
-    // @todo: endianness and alignment
-    std::stringstream ss;
-    std::int32_t size24( color24.size() );
-    std::int32_t size08( alpha08.size() );
-    ss.write( (const char *)&size24, 4 );
-    ss.write( (const char *)&size08, 4 );
-    ss.write( "pug1", 4 );
-    return ss.good() ? ss.str() : std::string();
-}
+image decode_png( const void *data, unsigned size );
+image decode_pug( const void *data, unsigned size );
 
-bool rdfooter( const std::string &filename, std::int32_t &off24, std::int32_t &len24, std::int32_t &off08, std::int32_t &len08 ) {
-    std::ifstream ifs( filename.c_str(), std::ios::binary|std::ios::ate );
-    if( ifs.good() ) {
-        std::int32_t FEOF = std::int32_t( ifs.tellg() );
-        if( FEOF >= 12 ) {
-            ifs.seekg( -12, std::ios_base::end );
-            ifs.read( (char *)&len24, 4 );
-            ifs.read( (char *)&len08, 4 );
-            char info[5] = {};
-            ifs.read( (char *)info, 4 );
-            if( !strcmp( info, "pug1" ) ) {
-                off24 = FEOF - 12 - len08 - len24;
-                off08 = FEOF - 12 - len08;
-                return true;
-            }
+std::string encode_png( unsigned w, unsigned h, const void *data, unsigned stride  );
+std::string encode_pug( unsigned w, unsigned h, const void *data, unsigned quality );
+
+int main( int argc, const char **argv ) {
+
+    auto read_file = []( std::string &input, const std::string &stream ) -> bool {
+        std::stringstream ss;
+        std::ifstream ifs( stream, std::ios::binary );
+        return ss << ifs.rdbuf() ? (input = ss.str(), true) : (input = std::string(), false);
+    };
+
+    auto write_file = []( const std::string &filename, const std::string &data ) -> bool {
+        if( !data.empty() ) {
+            std::ofstream ofs( filename.c_str(), std::ios::binary );
+            ofs.write( &data[0], data.size() );
+            return ofs.good();                
         }
-    }
-    off24 = len24 = off08 = len08 = 0;
-    return false;
-}
-
-int main( int argc, const char **_argv ) {
+        return false;
+    };
 
     if( argc < 3 ) {
         std::cout
-            << _argv[0] << " v" VERSION " - https://github.com/r-lyeh/pug"   << std::endl << std::endl
-            << "Usage: " << _argv[0] << " input.png output.pug [0..100]        (default quality: 70)" << std::endl
-            << "Usage: " << _argv[0] << " input.pug output.png" << std::endl;
+            << argv[0] << " v" VERSION " - https://github.com/r-lyeh/pug" << std::endl << std::endl
+            << "Usage: " << argv[0] << " input.png output.pug [0..100]        (default quality: 70)" << std::endl
+            << "Usage: " << argv[0] << " input.pug output.png" << std::endl;
         return -1;
     }
 
     unsigned quality = 70;
-    if( argc >= 4 ) quality = std::strtoul( _argv[3], 0, 0 );
+    if( argc >= 4 ) quality = std::strtoul( argv[3], 0, 0 );
     if( quality > 100 ) quality = 100;
 
-    std::string argv[] = { _argv[0], _argv[1], _argv[2] };
-    std::string ext = argv[1].substr( argv[1].find_last_of('.') + 1 );
+    std::string args[] = { argv[0], argv[1], argv[2] };
+    std::string ext = args[1].substr( args[1].find_last_of('.') + 1 );
 
-    if( ext.size() == 3 &&
-        std::tolower( ext[0] ) == 'p' &&
-        std::tolower( ext[1] ) == 'u' &&
-        std::tolower( ext[2] ) == 'g' ) {
+    image img;
+    std::string input;
+    std::string output;
 
-        std::string data = readfile( argv[1] );
+    if( !read_file( input, args[1] ) ) {
+        std::cerr << "cannot read input file" << std::endl;
+        return -1;
+    }
 
-        std::int32_t off24, len24;
-        std::int32_t off08, len08;
-        if( !rdfooter( argv[1], off24, len24, off08, len08 ) ) {
-            return std::cout << "bad input file: " << argv[1] << std::endl, -1;            
+    if( ext == "pug" ) {
+        img = decode_pug( &input[0], input.size() );
+        output = encode_png( img.w, img.h, img.pixels.data(), 4 );
+    }
+    else {
+        img = decode_png( &input[0], input.size() );
+        output = encode_pug( img.w, img.h, img.pixels.data(), quality );
+    }
+
+    if( !write_file( args[2], output) ) {
+        std::cerr << "cannot write output file" << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+
+// libpug
+
+int32_t swapbe( int32_t v ) {
+    // swap bytes (on big endian architectures)
+    union autodetect {
+        int word;
+        char byte[ sizeof(int) ];
+        autodetect() : word(1) 
+        {}
+    } _;
+    bool is_big = _.byte[0] == 0;
+    if( is_big ) {
+        unsigned char *p = (unsigned char *)&v;
+        std::swap( p[0], p[3] );
+        std::swap( p[1], p[2] );
+    }
+    return v;
+};
+
+image decode_png( const void *ptr, size_t size ) {
+    int w = 0, h = 0, bpp = 0;
+    stbi_uc *imageuc = stbi_load_from_memory( (const unsigned char *)ptr, size, &w, &h, &bpp, 4 );
+    if( imageuc ) {
+        // convert to image class
+        image img { w, h, std::vector<unsigned char>( w * h * 4 ) };
+        memcpy( img.pixels.data(), imageuc, w * h * 4 );
+        stbi_image_free( imageuc );
+        return img;
+    } else {
+        return image { 0, 0 };        
+    }
+}
+
+image decode_jpg( const void *ptr, size_t size ) {
+    return decode_png( ptr, size );
+}
+
+image decode_pug( const void *ptr, size_t size ) {
+    image img = decode_jpg( ptr, size );
+    // if .pug file, then decode alpha
+    if( img.pixels.size() && 0 == memcmp( "pug1", (const char *)ptr + size - 4, 4 ) ) {
+        const int32_t color_size = swapbe( *(const int32_t *)((const char *)ptr + size - 12) );
+        const int32_t alpha_size = swapbe( *(const int32_t *)((const char *)ptr + size - 8) );
+        int w2 = 0, h2 = 0, bpp2 = 0;
+        stbi_uc *alpha = stbi_load_from_memory( (const unsigned char *)ptr + color_size, alpha_size, &w2, &h2, &bpp2, 1 );
+        if( alpha ) {
+            for( unsigned it = 0, end = w2 * h2; it < end; ++it ) {
+                img.pixels[ it * 4 + 3 ] = alpha[ it ];
+            }
+            stbi_image_free( alpha );                        
         }
+    }                
+    return img;
+}
 
-        spot::image color( &data[off24], len24 );
-        spot::image alpha( &data[off08], len08 );
+std::string encode_png( unsigned w, unsigned h, const void *data, unsigned stride ) {
+    if( w && h && data && stride ) {
+        auto mode = LCT_RGBA;
+        /**/ if( stride == 3 ) mode = LCT_RGB;
+        else if( stride == 2 ) mode = LCT_GREY_ALPHA;
+        else if( stride == 1 ) mode = LCT_GREY;
+        unsigned char* png;
+        size_t pngsize;
+        unsigned bpp = 8;
+        unsigned error = lodepng_encode_memory_std( &png, &pngsize, (const unsigned char *)data, w, h, mode, bpp );
+        if( !error && pngsize ) {
+            std::string buf;
+            buf.resize(pngsize);
+            memcpy(&buf[0],png,pngsize);
+            free(png);
+            return buf;
+        }
+    }
+    return std::string();
+}
 
-        for( unsigned h = 0; h < color.h; ++h ) {
-            for( unsigned w = 0; w < color.w; ++w ) {
-                color.at(w,h).a = alpha.at(w,h).l;
+std::string encode_jpg( unsigned w, unsigned h, const void *data, unsigned quality ) {
+    if( w && h && data && quality ) {
+        std::string buf( 1024 + w * h * 3, '\0' );
+        jpge::params p;
+        p.m_quality = (int)quality;
+        p.m_two_pass_flag = true; // slower but slighty smaller
+        int buf_size = (int)buf.size();
+        if( jpge::compress_image_to_jpeg_file_in_memory(&buf[0], buf_size, w, h, 4, (const jpge::uint8 *)data, p) ) {
+            if( buf_size > 0 ) {
+                buf.resize((unsigned)(buf_size));
+                return buf;
             }
         }
-
-        return color.save_as_png( argv[2] ) ? 0 : -1;
     }
-    else
-    {
-        spot::image color( argv[1] );
-        color = color.bleed();
-
-        std::string color24 = color.encode_as_jpg( quality );
-        std::string alpha08 = color.encode_as_png( false, false, 8 );
-
-        std::ofstream ofs( argv[2], std::ios::binary );
-        if( ofs.good() ) {
-            ofs << color24 << alpha08 << mkfooter( color24, alpha08 );
-        }
-
-        return ofs.good() ? 0 : -1;
-    }
-
-    std::cout << "cannot determine input file" << std::endl;
-    return -1;
+    return std::string();
 }
+
+std::string encode_pug( unsigned w, unsigned h, const void *data, unsigned quality ) {
+    if( w && h && data && quality ) {
+        // encode color as jpg
+        std::string jpg = encode_jpg( w, h, data, quality );
+        // encode alpha as png (gray, lum8)
+        std::vector<unsigned char> alpha( w * h );
+        unsigned char *ptr = ((unsigned char *)data) + 3;
+        for( unsigned x = 0; x < w * h; ++x, ptr += 4 ) alpha[ x ] = *ptr;
+        std::string png = encode_png( w, h, alpha.data(), 1 );
+        // glue and footer
+        int32_t size24 = swapbe( int32_t(jpg.size()) );
+        int32_t size08 = swapbe( int32_t(png.size()) );
+        if( size24 && size08 ) {
+            std::stringstream ss;
+            ss.write( &jpg[0], size24 );
+            ss.write( &png[0], size08 );
+            ss.write( (const char *)&size24, 4 );
+            ss.write( (const char *)&size08, 4 );
+            ss.write( "pug1", 4 );
+            if( ss.good() ) {
+                return ss.str();
+            }
+        }
+    }
+    return std::string();
+}     
+
+
